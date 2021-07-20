e1b62ca8494d54f93d0218a49a00ef42a6db3e0b
Pedro Gabriel <pedrogabriel12423@gmail.com>
2021-07-20 17:54:40 -0300

    Primeiros inserts no banco de dados Escola

---
+++ Faculdade/Escola - BD/02 - Inserindo registros nas tabelas.sql
@@ -0,0 +1,5 @@
+insert into professor(nomeProf, dataadmissao, codisc)	values('Crishane', null, 1);
+insert into professor(nomeProf, dataadmissao, codisc)	values('Damires', null, 1);
+insert into professor(nomeProf, dataadmissao, codisc)	values('Luciana', null, 2);
+
+select * from professor;
